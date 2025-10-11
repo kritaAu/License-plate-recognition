cd9fd2e85304a6d71db40a8ffb81b6c6ca0991b3
ktp <kritapat.au@gmail.com>
2025-10-12 00:18:49 +0700

    OCR fix

--- BackEnd/OCR_ai.py
+++ BackEnd/OCR_ai.py
@@ -4,9 +4,9 @@ import os
 import re
 from utils import *
 
+
+
 client = OpenAI()
-
-
 def read_plate(img_b64: str = None, image_path: str = None):
 
     filename = os.path.basename(image_path)
@@ -21,60 +21,84 @@ def read_plate(img_b64: str = None, image_path: str = None):
     else:
         print("ไม่ตรง pattern")
 
-    response = client.responses.create(
-        model="gpt-4o",
-        input=[
-            {
-                "role": "system",
-                "content": (
-                    """
-                    คุณคือ OCR สำหรับภาพ “ป้ายทะเบียนมอเตอร์ไซค์ของไทย” (ครอปเฉพาะป้าย)
-                    รูปแบบบรรทัด:
-                    1) plate_top: อักษรไทย 1-2 ตัว + ตัวเลข 1-4 หลัก
-                    - ถ้าอักษรมากกว่า 2 ตัว ตัวแรกจะเป็นตัวเลขเสมอ
-                    - ตัวอย่าง: กข123, 1กก1234, 2กฟ2456, 23กง1256
-                    - อักขระสับสนที่พบบ่อย: ฐ↔ร, 0↔O↔อ, 1↔I↔l
-                    2) province: ชื่อจังหวัดจริงของไทยเท่านั้น (เช่น กรุงเทพมหานคร, เชียงใหม่, ขอนแก่น)
-                    3) plate_bottom: ตัวเลขล้วน 1-4 หลัก (มีเฉพาะบางป้าย)
-
-                    กติกา:
-                    - ไม่มั่นใจตัวไหน ใส่ “?” แทนตำแหน่งนั้น เช่น ก?123
-                    - ห้ามเดาหรือเติมจังหวัดถ้าไม่เห็นชัด
-                    - ออกผลเป็น JSON เท่านั้น
-                    - ค่า plate = plate_top ต่อด้วย plate_bottom (ถ้าไม่มีบรรทัดล่าง ให้ใช้เฉพาะ plate_top)
-                    -ถ้าไม่มีป้ายทะเบียนหรือภาพอะไรไม่รู้ให้plate เป็น "ไม่มีป้ายทะเบียน" province เป็น Null ยกเว้น time,direction,camera จะต้องมีเสมอ
-                    Output JSON:
-                    {
-                    "plate": "<plate_top(+plate_bottom)>",
-                    "province": "<provinceหรือเว้นว่างถ้าไม่เห็น>",
-                    "time":"<datetime จากไฟล์>"
-                    "direction":"<IN or OUT>"
-                    "camera":<number>
-                    }
-                    """
-                ),
-            },
-            {
-                "role": "user",
-                "content": [
-                    {
-                        "type": "input_text",
-                        "text": f'อ่านป้ายทะเบียนแล้วออก JSON โดยให้ datetime = "{iso_dt}" และ direction = "{direction}"และ Camera = "{cam}"',
-                    },
-                    {
-                        "type": "input_image",
-                        "image_url": f"data:image/jpeg;base64,{img_b64}",
-                        "detail": "low",
-                    },
-                ],
-            },
-        ],
-    )
     try:
-        txt = response.output_text.strip()
-        txt = re.sub(
-            r"^\s*```(?:json)?\s*|\s*```\s*$", "", txt, flags=re.IGNORECASE | re.DOTALL
+        
+        response = client.responses.create(
+            model="gpt-4o",
+            input=[
+                {
+                    "role": "system",
+                    "content": (
+                        """
+                        คุณคือ OCR สำหรับภาพ “ป้ายทะเบียนมอเตอร์ไซค์ของไทย” (ครอปเฉพาะป้าย)
+                        รูปแบบบรรทัด:
+                        1) plate_top:มีได้ 3 ตัวเท่านั้น 
+                        — ถ้ามีเลข เลขอยู่หน้าสุด 
+                        — สองตัวขวาต้องเป็นอักษรไทย
+                        -มีเลขซ้ายสุด → 1กก, 2ขข, 3พร, 9ธน
+                        -ไม่มีเลข → กข, กทม, พร, นค
+                        - อักขระสับสนที่พบบ่อย: ฐ↔ร, 0↔O↔อ, 1↔I↔l
+                        2) province: ชื่อจังหวัดจริงของไทยเท่านั้น (เช่น กรุงเทพมหานคร, เชียงใหม่, ขอนแก่น)
+                        3) plate_bottom: ตัวเลขล้วน 1-4 หลัก
+
+                        กติกา:
+                        - ไม่มั่นใจตัวไหน ใส่ “?” แทนตำแหน่งนั้น เช่น ก?123
+                        - ห้ามเดาหรือเติมจังหวัดถ้าไม่เห็นชัด
+                        - ออกผลเป็น JSON เท่านั้น
+                        - ค่า plate = plate_top ต่อด้วย plate_bottom (ถ้าไม่มีบรรทัดล่าง ให้ใช้เฉพาะ plate_top)
+                        -ถ้าไม่มีป้ายทะเบียนหรือภาพอะไรไม่รู้ให้plate เป็น "ไม่มีป้ายทะเบียน" province เป็น Null ยกเว้น time,direction,camera จะต้องมีเสมอ
+                        Output JSON:
+                        {
+                        "plate": "<plate_top(+plate_bottom)>",
+                        "province": "<provinceหรือเว้นว่างถ้าไม่เห็น>",
+                        "time":"<datetime จากไฟล์>"
+                        "direction":"<IN or OUT>"
+                        "camera":<number>
+                        }
+                        """
+                    ),
+                },
+                {
+                    "role": "user",
+                    "content": [
+                        {
+                            "type": "input_text",
+                            "text": f'อ่านป้ายทะเบียนแล้ว output เป็น JSON โดยให้ datetime = "{iso_dt}" และ direction = "{direction}"และ Camera = "{cam}"',
+                        },
+                        {
+                            "type": "input_image",
+                            "image_url": f"data:image/jpeg;base64,{img_b64}",
+                            "detail": "low",
+                        },
+                    ],
+                },
+            ],
         )
-        return json.loads(txt)
-    except Exception:
-        return {"raw_text": response.output_text}
+        try:
+            txt = response.output_text.strip()
+            txt = re.sub(
+                r"^\s*```(?:json)?\s*|\s*```\s*$", "", txt, flags=re.IGNORECASE | re.DOTALL
+            )
+            return json.loads(txt)
+        except Exception as e:
+            print(f"[ERROR OCR] {e}")
+            return{
+                        "time": iso_dt,
+                        "plate": None,
+                        "province": None,
+                        "direction": direction,
+                        "blob": None,
+                        "camera": cam,
+                        "vehicle_id": None,
+                    }
+    except Exception as e:
+            print(f"[ERROR OCR] {e}")
+            return{
+                        "time": iso_dt,
+                        "plate": None,
+                        "province": None,
+                        "direction": direction,
+                        "blob": None,
+                        "camera": cam,
+                        "vehicle_id": None,
+                    }
